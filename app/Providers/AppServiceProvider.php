a1ca24d4cda3c4eee51aecce07e240bec33ce034
marizke01 <marizke.mega.u@students.esqbs.ac.id>
2026-01-17 10:36:42 +0700

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Paksa semua URL (asset, route, dll) jadi https di production
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
